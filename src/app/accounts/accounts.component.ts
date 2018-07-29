53daa3ed7b0c1ff0956ac309c1566b4cb2e08f88
aducin <albert.ducin@gmail.com>
2018-07-29 13:11:20 +0200

    indentions

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -89,51 +89,51 @@ export class AccountsComponent implements OnInit {
 	messageDisplay(data) {
 		this.messageShow = data.display;
 		this.messageType = data.type;
-  		this.messageValue = data.value;
+		this.messageValue = data.value;
 	}
 
 	open(action) {
-        let data = {
-            action,
-            obj:<Account>{},
-            saveAllow: false,
-            state: this.config.accountState,
+		let data = {
+			action,
+			obj:<Account>{},
+			saveAllow: false,
+			state: this.config.accountState,
 			title: undefined,
 			token: this.tokenService.getToken(),
-            type: this.config.accountType,
-            cashTime: undefined,
-            receiptTime: undefined
-        };
+			type: this.config.accountType,
+			cashTime: undefined,
+			receiptTime: undefined
+		};
 		data.title = action === 'add' ? this.config.accountTitle[0] : this.config.accountTitle[1];
-        if (action === 'add') {
-            data.obj.closed = -1;
-            data.obj.type = -1;
+		if (action === 'add') {
+			data.obj.closed = -1;
+			data.obj.type = -1;
 			if (CheckEmpty(data.state) === -1) {
 				data.state.unshift(this.config.choose);	
 			}
-            if (CheckEmpty(data.type) === -1) {
+			if (CheckEmpty(data.type) === -1) {
 				data.type.unshift(this.config.choose);
 			}
-        } else {
-            var check = this.list.findIndex( el => el.id === this.selected );
-            if (check !== -1) {
-                data.obj = this.list[check];
-                if (data.obj.cashTime !== undefined) {
-                    data.cashTime = SplitDate(data.obj.cashTime);
-                }
-                if (data.obj.receiptTime !== undefined) {
+		} else {
+			var check = this.list.findIndex( el => el.id === this.selected );
+			if (check !== -1) {
+				data.obj = this.list[check];
+				if (data.obj.cashTime !== undefined) {
+					data.cashTime = SplitDate(data.obj.cashTime);
+				}
+				if (data.obj.receiptTime !== undefined) {
 					data.cashTime = SplitDate(data.obj.receiptTime);
-                }
-            }
-        }
+				}
+			}
+		}
 		const modalRef = this.modalService.open(AccountModal, { windowClass: 'current-modal' });
 		modalRef.componentInstance.data = data;
 		modalRef.result.then((refresh) => {
 			if (refresh) {
 				this.service.setInitialState();
 			}
-	  	}, (reason) => {
-	  	});
+		}, (reason) => {
+		});
 	}
 
 	postMessageAction(data) {
